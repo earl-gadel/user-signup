d54f09a33a42afc26191087365beb8248ae938a6
Earl Gadel <earljg@gmail.com>
2018-04-04 11:59:40 -0500

    started username error in signup.html and added corresponding variables to main.py

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
+    username = request.form['username']
+    password = request.form['password']
+    verify = request.form['verify']
+    if username == password == verify == '':
+    #error displayed when user leaves any of the following fields empty: username, password, verify password
+        error = "Please fill in this field"
+        return redirect("/?error=" + error)
     return render_template('signup.html')
 
 
