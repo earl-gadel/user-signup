b2454ccda768534cebff685ede4313d2c8ddd1c2
Earl <earljg@gmail.com>
2018-04-04 20:15:46 -0500

    fixed 400 server error, working on printing error message

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ app.config['DEBUG'] = True
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
-    username = request.form['username']
-    password = request.form['password']
-    verify = request.form['verify']
-    if username == password == verify == '':
+    if request.method == 'POST':
+        username = request.form['username']
+        password = 't'#request.form['password']
+        verify = 't' #request.form['verify']
+        if username == '':
 
     #error displayed when user leaves any of the following fields empty: username, password, verify password
-        error = "Please fill in this field"
-        return redirect("/?error=" + error)
+            error = "Please fill in this field"
+            return redirect("/?error=" + error)
+        return render_template('signup.html', username=username, password=password, verify=verify)
     return render_template('signup.html')
-
 
 app.run()
