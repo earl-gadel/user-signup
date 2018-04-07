f64f172ffd3ea67e71361077c3f9262715a58fda
Earl <earljg@gmail.com>
2018-04-06 19:08:55 -0500

    broke it trying to get the for loops working

--- main.py
+++ main.py
@@ -6,17 +6,24 @@ app.config['DEBUG'] = True
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        verify = request.form['verify']
+    error_user = ''
+    error_pass = ''
+    error_verify = ''
+
+    #if request.method == 'POST':
+    username = request.form['username']
+    #password = request.form['password']
+    #verify = request.form['verify']
+    for i in username:
         if username == '':
             error_user = "That's not a valid username"
+            break
 
-        elif password == '':
+    for i in password:
+        if password == '':
             error_pass = "That's not a valid password"
-
-            return render_template('signup.html', error_user=error_user, error_pass=error_pass)
+            break
+        #return render_template('signup.html', error_user=error_user, error_pass=error_pass)
         #elif len(password) < 3 or len(password) > 20:
             #error_pass = "That's not a valid password"
             #return render_template('signup.html', error_pass=error_pass)
@@ -26,6 +33,7 @@ def index():
 
 
         #return render_template('signup.html', username=username, password=password, verify=verify)
-    return render_template('signup.html')
+    return render_template('signup.html', error_user=error_user, error_pass=error_pass)
+
 
 app.run()
