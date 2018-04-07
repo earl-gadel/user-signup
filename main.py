5d242895eba95bd74f1412a75a7ec25f1080921b
Earl <earljg@gmail.com>
2018-04-07 10:48:55 -0500

    added welcome.html, fixed error checks and return input text

--- main.py
+++ main.py
@@ -16,29 +16,37 @@ def index():
         password = request.form['password']
         verify = request.form['verify']
         email = request.form['email']
-        #for i in username:
-        if username == '':
+
+        for i in username:
+            if i == '':
+                error_user = "That's not a valid username"
+            elif i == ' ':
+                error_user = "That's not a valid username"
+
+        if len(username) < 3 or len(username) > 20:
             error_user = "That's not a valid username"
-            #return render_template('signup.html', error_user=error_user)
 
-        #for i in password:
-        if password == '':
+        for i in password:
+            if i == '':
+                error_pass = "That's not a valid password"
+            elif i == ' ':
+                error_pass = "That's not a valid password"
+
+        if len(password) < 3 or len(password) > 20:
             error_pass = "That's not a valid password"
-            #return render_template('signup.html', error_user=error_user, error_pass=error_pass)
 
-        if verify == '':
+        if verify != password:
             error_verify = "Passwords don't match"
-            #return render_template('signup.html', error_user=error_user, error_pass=error_pass, error_verify=error_verify)
 
         for i in email:
-            if i == '@':
+            if '@' not in email and '.' not in email:
                 error_email = 'Not valid email'
-                return render_template('signup.html', error_user=error_user, error_pass=error_pass,
-                                       error_verify=error_verify, error_email=error_email)
+        return render_template('signup.html', error_user=error_user, error_pass=error_pass,
+                                       error_verify=error_verify, error_email=error_email, username=username, email=email)
 
 
         #return render_template('signup.html', username=username, password=password, verify=verify)
-    return render_template('signup.html', error_user=error_user, error_pass=error_pass)
+    return render_template('signup.html')
 
 
 app.run()
