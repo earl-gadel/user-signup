26b438bd9c71f15ddf5889402d2e05e521eda7a4
Earl <earljg@gmail.com>
2018-04-07 11:09:45 -0500

    almost there, working on the error class in css

--- main.py
+++ main.py
@@ -35,12 +35,15 @@ def index():
         if len(password) < 3 or len(password) > 20:
             error_pass = "That's not a valid password"
 
-        if verify != password:
+        if verify != password or len(verify) < 3 or len(verify) > 20 or verify == '':
             error_verify = "Passwords don't match"
 
         for i in email:
             if '@' not in email and '.' not in email:
-                error_email = 'Not valid email'
+                error_email = "That's not a valid email"
+
+        if error_user == error_email == error_verify == error_email == '':
+            return render_template('welcome.html', username=username)
         return render_template('signup.html', error_user=error_user, error_pass=error_pass,
                                        error_verify=error_verify, error_email=error_email, username=username, email=email)
 
