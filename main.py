c9fb884971a3a45788d2c3a6f2ca1db01db21fc3
Earl <earljg@gmail.com>
2018-04-06 21:08:17 -0500

    the first errors are working, still need to check password and email conditionals

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ def index():
     error_user = ''
     error_pass = ''
     error_verify = ''
+    error_email = ''
 
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         verify = request.form['verify']
+        email = request.form['email']
         #for i in username:
         if username == '':
             error_user = "That's not a valid username"
@@ -22,14 +24,17 @@ def index():
         #for i in password:
         if password == '':
             error_pass = "That's not a valid password"
-            return render_template('signup.html', error_user=error_user, error_pass=error_pass)
-        #return render_template('signup.html', error_user=error_user, error_pass=error_pass)
-        #elif len(password) < 3 or len(password) > 20:
-            #error_pass = "That's not a valid password"
-    #return render_template('signup.html', error_pass=error_pass)
-        #elif verify == '':
-            #error_verify = "Passwords don't match"
-            #return render_template('signup.html', error_verify=error_verify)
+            #return render_template('signup.html', error_user=error_user, error_pass=error_pass)
+
+        if verify == '':
+            error_verify = "Passwords don't match"
+            #return render_template('signup.html', error_user=error_user, error_pass=error_pass, error_verify=error_verify)
+
+        for i in email:
+            if i == '@':
+                error_email = 'Not valid email'
+                return render_template('signup.html', error_user=error_user, error_pass=error_pass,
+                                       error_verify=error_verify, error_email=error_email)
 
 
         #return render_template('signup.html', username=username, password=password, verify=verify)
